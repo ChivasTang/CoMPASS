e1246aa9c42b031a2c45c8465e73c677db25c1af
ChivasTang <tangzh1983@gmail.com>
2018-11-03 14:31:17 +0900

    20181103

--- src/systemi/Skha002B1.java
+++ src/systemi/Skha002B1.java
@@ -3,8 +3,6 @@ package systemi;
 import java.io.IOException;
 import java.sql.SQLException;
 
-import systemi.PFOReceiveCommon;
-
 /**
  * 名称：Skha002B1
  * 説明：保全計画取込受信を起動する。
@@ -17,7 +15,7 @@ import systemi.PFOReceiveCommon;
  */
 
 // 保全計画取込受信のmain実行クラス
-public class Skha002B1 extends PFOReceiveCommon {
+public class Skha002B1 extends PFOReceiveCommon{
 
     // 販売計画(中長期予算)受信main
     public static void main(String[] args) {
@@ -47,7 +45,7 @@ class Skha002B1Start extends PFOReceiveCommon {
     // 受信共通実行メソッド
     public void start() {
         // 引数配列作成
-        String[] str = { "SKHA002B1" };
+        String[] str = { "SKHA002B1","","","" };
         try {
             // 受信処理共通メソッドを呼び出し、受信共通処理を実行する。（引数：バッチID）
             super.exec(str);
@@ -85,48 +83,6 @@ class Skha002B1Start extends PFOReceiveCommon {
 
         // 相関チェック用パラメータ初期化
         String[][] iCheckParam = new String[11][8];
-        for (int i = 0; i < 11; i++) {
-            for (int j = 0; j < 8; j++) {
-                iCheckParam[i][j] = "";
-            }
-        }
-
-        // 空文字チェック
-        // "1"：チェック対象、""（空白）：チェック対象外
-        iCheckParam[0][0] = "1"; // 工場コード
-        iCheckParam[0][1] = "1"; // 設備コード
-        iCheckParam[0][2] = "1"; // 定修年月
-        iCheckParam[0][3] = "1"; // 開始日時
-        iCheckParam[0][4] = "1"; // 終了日時
-        iCheckParam[0][5] = "1"; // 定期修理区分
-
-        // 文字種チェック
-        // "A"：英数字、"D"：日付、"N"：数値文字、""（空白）：チェック対象外
-        iCheckParam[1][0] = "A"; // 工場コード
-        iCheckParam[1][1] = "A"; // 設備コード
-        iCheckParam[1][2] = "D"; // 定修年月
-        iCheckParam[1][3] = "D"; // 開始日時
-        iCheckParam[1][4] = "D"; // 終了日時
-        iCheckParam[1][5] = "A"; // 定期修理区分
-
-        // 桁数チェック
-        // （数値）：文字列、"N"：数値文字、""（空白）：チェック対象外
-        iCheckParam[4][0] = "25"; // 工場コード
-        iCheckParam[4][1] = "25"; // 設備コード
-        iCheckParam[4][5] = "25"; // 定期修理区分
-        iCheckParam[4][6] = "50"; // 定期修理区分名称
-        iCheckParam[4][7] = "50"; // 備考
-
-        // 日付チェック ""（空白）：チェック対象外
-        iCheckParam[5][2] = "yyyyMM"; // 定修年月
-        iCheckParam[5][3] = "yyyyMMddHHmm"; // 開始日時
-        iCheckParam[5][4] = "yyyyMMddHHmm"; // 終了日時
-
-        // 相関チェック
-        // "01"～"14"：チェック対象（マスタ種別）、""（空白）：チェック対象外
-        // ???質問:"23"、"24"がチェック対象かどうか確認中
-        iCheckParam[9][0] = "23"; // 工場コード
-        iCheckParam[9][1] = "24"; // 設備コード
 
         return iCheckParam;
     }
